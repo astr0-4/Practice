3bcdc236feef550484329fdfabcad5930517e8ba
Alex Stroulger <alexstroulger@gmail.com>
2015-01-25 11:44:43 -0500

    Initial Commit

---
+++ Practice/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Practice
+//
+//  Created by Alex on 2015-01-25.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
